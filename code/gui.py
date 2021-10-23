88ac4c6bd1a766e49959f1d80d075cb338f87757
Mark <no email>
2021-10-23 19:58:57 -0300

    jdask

--- code/gui.py
+++ code/gui.py
@@ -88,7 +88,7 @@ class GuiMain(QDialog):
 
     def load_config(self,produto):
         try:
-            tree = ET.parse(str(produto) +'.xml')
+            tree = ET.parse("'.\config\'" + str(produto) +'.xml')
             root = tree.getroot()
         
             for x in root.findall('camera'):
