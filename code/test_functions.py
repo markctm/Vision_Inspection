c2d39c004d47e234cd9982a2b739a7132a76c349
Mark <no email>
2021-10-25 14:13:46 -0300

    tirando o mes da jogada

--- code/test_functions.py
+++ code/test_functions.py
@@ -165,11 +165,11 @@ class Test():
         
         if(score>int(tresh)):
             cv2.putText(img1, "PASS - LABEL DETECTED", (50, 400), fonte, 3, (0,255,0), 3, cv2.LINE_AA)
-            send_test_result("P")    
+            #send_test_result("P")    
         
         else:
             cv2.putText(img1, "FAIL- NO LABEL", (50, 400), fonte, 3, (0,0,255), 3, cv2.LINE_AA)
-            send_test_result("F")    
+            #send_test_result("F")    
         cv2.putText(img1, "Score:" + str(score), (50, 430), fonte, 1, (125,255,255), 1, cv2.LINE_AA)
         return score
   
