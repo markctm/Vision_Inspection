96d74f22b02c0fd1459c0719ef6f970f3e6e8f25
Mark <no email>
2021-10-28 22:51:20 -0300

    alksdj

--- code/gui.py
+++ code/gui.py
@@ -131,7 +131,7 @@ class GuiMain(QDialog):
 
        if self.timer2.isActive()==False:
 
-            self.timer2.setInterval(10000)
+            self.timer2.setInterval(1000)
             self.timer2.setSingleShot(True) 
             self.timer2.start() 
 
