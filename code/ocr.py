7df1a0299915ab5f945b3202e5b2b1cc4514778a
Mark <no email>
2021-10-30 17:06:30 -0300

    dcc

--- code/ocr.py
+++ code/ocr.py
@@ -4,22 +4,20 @@ import cv2
 
 
 image = cv2.imread('picture.jpg')
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+image = cv2.resize(image, (640, 480), interpolation=cv2.INTER_CUBIC)
+#cv2.imshow("Image", image)
+#cv2.waitKey(0)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 #cv2.imshow("Image", blurred)
-cv2.waitKey(0)
+#cv2.waitKey(0)
 
-(T, threshInv) = cv2.threshold(gray, 230, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-cv2.imshow("Simple Thresholding", threshInv)
-cv2.waitKey(0)
+thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 10)
+#cv2.imshow("Image", thresh)
 
-text = pytesseract.image_to_string(threshInv)
+#v2.imshow("Simple Thresholding", thresh)
+#cv2.waitKey(0)
+
+text = pytesseract.image_to_string(thresh)
 print(text)
-
-
-
-
-
