1d8390f5debf4c394ed2b51e4d5e6b4d3b630b37
Mark <no email>
2021-10-30 15:34:23 -0300

    wait

--- code/ocr.py
+++ code/ocr.py
@@ -5,13 +5,16 @@ import cv2
 
 image = cv2.imread('picture.jpg')
 cv2.imshow("Image", image)
+cv2.waitKey(0)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 cv2.imshow("Image", blurred)
+cv2.waitKey(0)
 
 (T, threshInv) = cv2.threshold(blurred, 230, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 cv2.imshow("Simple Thresholding", threshInv)
+cv2.waitKey(0)
 
 text = pytesseract.image_to_string(threshInv)
 print(text)
