020fda99a585e680fde5f52f7bf11f381d34097c
Mark <no email>
2021-10-30 14:41:12 -0300

    adicionando pytesseract

--- code/gui.py
+++ code/gui.py
@@ -131,8 +131,7 @@ class GuiMain(QDialog):
 
        if self.timer2.isActive()==False:
 
-            #DEBOUNCE MOUSE 
-            
+            # DEBOUNCE MOUSE        
             self.timer2.setInterval(1000)
             self.timer2.setSingleShot(True) 
             self.timer2.start() 
