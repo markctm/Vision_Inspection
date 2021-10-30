8ccf659fa41fd4d1ca6b77f9eb3ef22802c9dc53
Mark <no email>
2021-10-30 19:41:23 -0300

    test ocr

---
+++ code/Testplan/SOLO-OCR_1_testplan.tpl
@@ -0,0 +1,2 @@
+OCR_Search_FW_Version:"VERSION3221",0
+blank:1,2
--- code/test_functions.py
+++ code/test_functions.py
@@ -3,7 +3,7 @@ import cv2
 from preprocessing import Preprocess
 from mes import *
 from datetime import datetime
-
+import pytesseract 
 
 class Test():
  
@@ -183,6 +183,40 @@ class Test():
         return score
   
         
+    def OCR_Search_FW_Version(self,string,restest,img1,imgref):
+        
+        image = cv2.resize(img1, (640, 480), interpolation=cv2.INTER_CUBIC)
+        #cv2.imshow("Image", image)
+        #cv2.waitKey(0)
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+        #cv2.imshow("Image", blurred)
+        #cv2.waitKey(0)
+
+        thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 10)
+        #cv2.imshow("Image", thresh)
+
+        #v2.imshow("Simple Thresholding", thresh)
+        #cv2.waitKey(0)
+
+        text = pytesseract.image_to_string(thresh)
+
+        print(text)
+        text2=text.replace('.', '')
+        text2=text2.replace(' ', '')
+        text2=text2.upper()
+
+        fonte = cv2.FONT_HERSHEY_SIMPLEX 
+
+        if string in text2:
+            print("RESULT PASS" + str(string))
+            cv2.putText(img1, "RESULT PASS" + str(string), (50, 400), fonte, 3, (0,255,0), 3, cv2.LINE_AA) 
+        else:
+            print("RESULT FAIL")
+            cv2.putText(img1, "RESULT FAIL"+ str(string), (50, 400), fonte, 3, (0,0,255), 3, cv2.LINE_AA)
+
+    
     def blank(self,x,y):
        # print("Hello Mundo!")
        # print(x)
