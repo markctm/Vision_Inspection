4a24ccd3017b9b460a7ecb9bae79c3dae164f748
Mark <no email>
2021-11-09 20:22:07 -0300

    sajkhdk

--- code/test_functions.py
+++ code/test_functions.py
@@ -239,14 +239,14 @@ class Test():
             cv2.putText(img1, "OCR:" + str(text2), (50, 630), fonte, 1, (0,255,0), 1, cv2.LINE_AA)
             cv2.imwrite("./logs/" + str(SerialNumber)+ str(dt_string) + "_pass.jpg",img1)   
             #send_test_result("P")
-            #send_test_result_parser("P") 
+            send_test_result_parser("P") 
         else:
             print("RESULT FAIL")
             cv2.putText(img1, "RESULT FAIL FW"+ str(string), (50, 600), fonte, 2.5, (0,0,255), 3, cv2.LINE_AA)
             cv2.putText(img1, "OCR:" + str(text2), (50, 630), fonte, 1, (0,0,255), 1, cv2.LINE_AA)
             cv2.imwrite("./logs/" + str(SerialNumber)+ str(dt_string) + "_fail.jpg",img1)  
             #send_test_result("F")
-            #send_test_result_parser("F")
+            send_test_result_parser("F")
     
     def blank(self,x,y):
        # print("Hello Mundo!")
