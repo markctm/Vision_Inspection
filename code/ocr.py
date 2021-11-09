fd96dce60b5f87838e405841a7478d663fc057d7
Mark <no email>
2021-11-09 19:01:49 -0300

    teste ocr cut

--- code/ocr.py
+++ code/ocr.py
@@ -9,27 +9,24 @@ image = cv2.resize(image, (640, 480), interpolation=cv2.INTER_CUBIC)
 #cv2.imshow("Image", image)
 #cv2.waitKey(0)
 
-imagem_recorte1=np.empty((300,640))
-imagem_recorte1=image[100:399,0:639]
+imagem_recorte1=np.empty((200,640))
+imagem_recorte1=image[100:199,0:639]
 
 cv2.imshow("Image", imagem_recorte1)
 cv2.waitKey(0)
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-#cv2.imshow("Image", blurred)
-#cv2.waitKey(0)
+cv2.imshow("Image", blurred)
+cv2.waitKey(0)
 
 thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 10)
+
 #cv2.imshow("Image", thresh)
 
-#v2.imshow("Simple Thresholding", thresh)
-#cv2.waitKey(0)
+cv2.imshow("Simple Thresholding", thresh)
+cv2.waitKey(0)
 
 text = pytesseract.image_to_string(thresh)
-
-
-
-
 
 print(text)
