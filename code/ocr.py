30a8e3bca0c3f00416e4b16dc31a43ba0279a214
Mark <no email>
2021-11-09 19:35:12 -0300

    asjdlska

--- code/ocr.py
+++ code/ocr.py
@@ -9,8 +9,8 @@ image = cv2.resize(image, (640, 480), interpolation=cv2.INTER_CUBIC)
 #cv2.imshow("Image", image)
 #cv2.waitKey(0)
 
-imagem_recorte1=np.empty((120,640))
-imagem_recorte1=image[50:169,0:639]
+imagem_recorte1=np.empty((110,640))
+imagem_recorte1=image[60:169,0:639]
 
 cv2.imshow("Image", imagem_recorte1)
 cv2.waitKey(0)
