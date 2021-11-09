76e4be23f5144a2c57a68c4f9504005f95957e23
Mark <no email>
2021-11-09 19:56:11 -0300

    fw variavel ocr

--- code/ocr.py
+++ code/ocr.py
@@ -19,7 +19,7 @@ cv2.waitKey(0)
 gray = cv2.cvtColor(imagem_recorte1, cv2.COLOR_BGR2GRAY)
 
 blurred = cv2.GaussianBlur(gray, (3, 3), 0)
-cv2.imshow("Image", blurred)
+cv2.imshow("Image", blurred)]
 cv2.waitKey(0)
 
 thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 33, 15)
