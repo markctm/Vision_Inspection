55b8c375fd2569002150237fc38a704adf07da6a
Mark <no email>
2021-11-15 23:58:25 -0300

    sdlakj

--- code/gui.py
+++ code/gui.py
@@ -151,6 +151,7 @@ class GuiMain(QDialog):
                 if self.tesplan_load==True:
 
                     set_data_to_test(self.TIS_url,self.customer,self.customer,self.Serial_Number,self.assembly_nummber,self.tester_name,self.operator_name,self.process_step,self.checkBox_calibration_mode.isChecked())                    
+                    res=None
                     if self.checkBox_calibration_mode.isChecked()==False:
                         res=check_ok_test()
 
